d238290cdf0c0e2a1755b90be0c37f0b38686cb0
PhoonThetPine <thetpine254@gmail.com>
2023-04-01 12:21:48 +0800

    Initial Commit

---
+++ Movie Booking App/Movie_Booking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Booking_AppApp.swift
+//  Movie Booking App
+//
+//  Created by Phoon Thet Pine on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Booking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
