ba091c83fbed7a4e0a396f9bb68976c86892751c
PhoonThetPine <thetpine254@gmail.com>
2023-04-21 19:52:06 +0800

    v1

---
+++ Movie Booking App/Data/TabData.swift
@@ -0,0 +1,16 @@
+//
+//  TabData.swift
+//  Movie Booking App
+//
+//  Created by Phoon Thet Pine on 1/4/23.
+//
+
+import Foundation
+
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
